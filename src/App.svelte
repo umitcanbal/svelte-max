7b327f83c3e21675980dc85fa92410d020775324
Ümitcan Bal <umitcanbal@gmail.com>
2021-09-19 02:16:27 +0200

    reactive variables

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,41 @@
 <script>
-	export let name;
+  let name = "ümitcan";
+  let surname = "bal";
+  let age = 29;
+
+  $: upperCaseName = name.toUpperCase(); // pass by reference
+  let upperCaseSurname = surname.toUpperCase(); // pass by value
+
+  $: console.log(`upperCaseName`, upperCaseName); // logs everytime the value changes
+  $: console.log(`upperCaseSurname`, upperCaseSurname); // logs only once
+
+  const changeName = () => {
+    name = "Ali";
+  };
+
+  const changeSurname = () => {
+    surname = "Yılmaz";
+  };
+
+  const incrementAge = () => {
+    age += 1;
+  };
+
+  $: if (age % 4 === 3) {
+    // logs whenever the condition is met
+    console.log(`age`, age);
+  }
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
+<h1>
+  My name is {upperCaseName}, my surname is {upperCaseSurname}, my age is {age}
+</h1>
+<button on:click={changeName}>Change Name</button>
+<button on:click={changeSurname}>Change Surname</button>
+<button on:click={incrementAge}>Change Age</button>
 
-<h1>Hello {name}!</h1>
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
